292a06a4bcf9d9c48843681ce111f71da96fe7b0
NiklasWyld <84245129+NiklasWyld@users.noreply.github.com>
2022-07-29 13:38:39 +0200

    Fixed database bug

--- BackEnd/DelEmployeeLogic.py
+++ BackEnd/DelEmployeeLogic.py
@@ -1,4 +1,6 @@
 from PyQt5.QtWidgets import QWidget, QMessageBox
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
 import Wydbid
 from Data import DataCombi
 
@@ -8,9 +10,14 @@ def delEmployeeFinal(username: str, password: str, widget: QWidget):
                             'All fields must be filled in!')
         return
 
-    session = DataCombi.session(bind=DataCombi.engine)
+    engine = create_engine(f'sqlite:///{Wydbid.company_location}database.db')
+    _session = sessionmaker()
+    base = declarative_base()
+    session = _session(bind=engine)
 
-    employee = session.query(DataCombi.Employee).filter(DataCombi.Employee.username == username).first()
+    class BaseEmployee(DataCombi.Employee, base): pass
+    print(username)
+    employee = session.query(BaseEmployee).filter(BaseEmployee.username == username).first()
     print(employee)
 
     return
